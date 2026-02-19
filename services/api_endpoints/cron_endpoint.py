805e5a6542b9fc9a1a287820ffa45999918d48d5
Alexey Vinogradov <avinogradov@gmail.com>
2026-02-19 21:07:09 +0300

    Debug

--- services/api_endpoints/cron_endpoint.py
+++ services/api_endpoints/cron_endpoint.py
@@ -20,6 +20,7 @@ def handle_cron_blueprint(auth_service: AuthService, calendar_service: CalendarS
     @calendar_blp.response(201, description="Job executed successfully")
     def handle_cron_api():
         print(request.json)
+        logger.info(request.get_data())
         if not auth_service.validate_api_key():
             return jsonify({'error': {'code': 401, 'message': 'Unauthorized'}}), 401
         
